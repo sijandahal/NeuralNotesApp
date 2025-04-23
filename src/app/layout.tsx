c422f2d7cd2b91fe6e59d7f02671d1222f7644fb
sijandahal <dahalsijan7@gmail.com>
2025-04-22 20:39:37 -0500

    added all files

---
+++ src/app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import "@/styles/globals.css";
+import { ThemeProvider } from "@/providers/ThemeProvider";
+import { Toaster } from "@/components/ui/toaster";
+import Header from "@/components/Header";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import AppSidebar from "@/components/AppSidebar";
+import NoteProvider from "@/providers/NoteProvider";
+
+export const metadata: Metadata = {
+  title: "Neural Notes",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NoteProvider>
+            <SidebarProvider>
+              <AppSidebar />
+
+              <div className="flex min-h-screen w-full flex-col">
+                <Header />
+
+                <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
+                  {children}
+                </main>
+              </div>
+            </SidebarProvider>
+
+            <Toaster />
+          </NoteProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
